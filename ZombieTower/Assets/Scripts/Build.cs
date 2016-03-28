f3ae95856c0395defe5165208b74fe5f16d82535
Jay Hynes <Jay Hynes>
2016-03-28 23:06:19 +0100

    Texture added, build started

---
+++ ZombieTower/Assets/Scripts/Build.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Build : MonoBehaviour {
+    
+    public GameObject tower;
+    
+    void OnMouseUpAsButton() {
+    
+    GameObject g = (GameObject)Instantiate(tower);
+    g.transform.position = transform.position + Vector3.up;
+    }
+}
