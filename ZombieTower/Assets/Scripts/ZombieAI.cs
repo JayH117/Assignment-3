3eee8707dab1018caece58a86fc2e046cb647d24
Jay Hynes <Jay Hynes>
2016-03-29 21:28:39 +0100

    Zombies move, attack

--- ZombieTower/Assets/Scripts/ZombieAI.cs
+++ ZombieTower/Assets/Scripts/ZombieAI.cs
@@ -4,15 +4,15 @@ using System.Collections;
 public class ZombieAI : MonoBehaviour {
 
 	void Start(){
-    GameObject zombie = GameObject.Find("Zombie");
-    if (zombie)
-        GetComponent<NavMeshAgent>().destination = zombie.transform.position;
+    GameObject home = GameObject.Find("Base");
+    if (home)
+        GetComponent<NavMeshAgent>().destination = home.transform.position;
     }
 	
 	// Update is called once per frame
 	void OnTriggerEnter(Collider co) {
     
-    if (co.name == "Zombie") {
+    if (co.name == "Base") {
         co.GetComponentInChildren<HealthScript>().decrease();
         Destroy(gameObject);
         }
