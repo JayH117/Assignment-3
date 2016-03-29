82abc36bce5a990773aeba2d1854a857b29b4224
Jay Hynes <Jay Hynes>
2016-03-29 23:15:51 +0100

    Added tower prefab + script

---
+++ ZombieTower/Assets/Scripts/Tower.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tower : MonoBehaviour {
+    // The Bullet
+    public GameObject bullet;
+    
+    // Rotation Speed
+    public float rotationSpeed = 35;
+    
+    void Update() {
+        transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed, Space.World);
+    }
+    
+    void OnTriggerEnter(Collider co) {
+        // Was it a Monster? Then Shoot it
+        if (co.GetComponent<ZombieAI>()) {
+            GameObject g = (GameObject)Instantiate(bullet, transform.position, Quaternion.identity);
+            g.GetComponent<Bullet>().target = co.transform;
+        }
+    }
+}
