ba8061907817682004d14d4a2fdfee98d6944cee
Jay Hynes <Jay Hynes>
2016-03-29 22:25:13 +0100

    Added Bullet prefab, script

---
+++ ZombieTower/Assets/Scripts/Bullet.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+   
+    public float speed = 10;
+    
+    public Transform target;    
+    
+    void FixedUpdate() {    
+  
+    if (target) {
+            
+        Vector3 dir = target.position - transform.position;
+        GetComponent<Rigidbody>().velocity = dir.normalized * speed;
+        } else {
+        
+        Destroy(gameObject);
+        }
+    }
+    
+    void OnTriggerEnter(Collider co) {
+        HealthScript health = co.GetComponentInChildren<HealthScript>();
+        if (health) 
+        {
+            health.decrease();
+            Destroy(gameObject);
+        }
+    }
+}
