432ff5874b64cb24cc67fb67857c6144c0f438c8
Jay Hynes <Jay Hynes>
2016-03-29 19:54:27 +0100

    Spawn added, zombie prefab

---
+++ ZombieTower/Assets/Scripts/ZombieAI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieAI : MonoBehaviour {
+
+	void Start(){
+    GameObject zombie = GameObject.Find("Zombie");
+    if (zombie)
+        GetComponent<NavMeshAgent>().destination = zombie.transform.position;
+    }
+	
+	// Update is called once per frame
+	void OnTriggerEnter(Collider co) {
+    
+    if (co.name == "Zombie") {
+        co.GetComponentInChildren<HealthScript>().decrease();
+        Destroy(gameObject);
+        }
+    }
+}
