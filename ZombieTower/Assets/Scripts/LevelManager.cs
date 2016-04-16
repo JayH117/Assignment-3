c7bfce701944c48d59cc7b732bfcbd97d1513689
Kev Furlong <kevin16furlong@gmail.com>
2016-04-16 21:31:02 +0100

    menu start

---
+++ ZombieTower/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+    public void LoadScene(string name)
+        {
+            Application.LoadLevel(name);
+        
+    }
+}
+//hello this is a comment
+//https://www.youtube.com/watch?v=vfi4ZYHR-MU
